12dc50509014dc58ab95b138226b35a186f6ac04
gangyounghyun9792 <s21020@gsm.hs.kr>
2022-04-27 19:54:23 +0900

    url name

--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 # Create your views here.
 def index(request):
-    quesiton_list = Question.objects.order_vy('-create_date')
+    quesiton_list = Question.objects.order_by('-create_date')
     context = {'question_list': quesiton_list}
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question' : question}
+    return render(request, 'pybo/question_detail.html', context)
